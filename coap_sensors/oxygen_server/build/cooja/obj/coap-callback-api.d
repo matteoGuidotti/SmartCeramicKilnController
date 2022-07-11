c87e85670836ccb38e99027c0c9083312e69ab3a
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-07-11 06:22:35 -0400

    percorso makefile border router

---
+++ coap_sensors/oxygen_server/build/cooja/obj/coap-callback-api.d
@@ -0,0 +1,89 @@
+build/cooja/obj/coap-callback-api.o: \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-callback-api.c \
+ /home/user/contiki-ng/os/net/app-layer/coap/module-macros.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-engine.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-constants.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-conf.h \
+ /home/user/contiki-ng/os/contiki.h \
+ /home/user/contiki-ng/arch/platform/cooja/./contiki-conf.h \
+ project-conf.h /home/user/contiki-ng/os/contiki-default-conf.h \
+ /home/user/contiki-ng/os/sys/process.h /home/user/contiki-ng/os/sys/pt.h \
+ /home/user/contiki-ng/os/sys/lc.h \
+ /home/user/contiki-ng/os/sys/lc-switch.h \
+ /home/user/contiki-ng/os/sys/cc.h /home/user/contiki-ng/os/sys/cc-gcc.h \
+ /home/user/contiki-ng/os/sys/autostart.h \
+ /home/user/contiki-ng/os/sys/process.h \
+ /home/user/contiki-ng/os/sys/timer.h \
+ /home/user/contiki-ng/os/sys/clock.h \
+ /home/user/contiki-ng/os/sys/ctimer.h \
+ /home/user/contiki-ng/os/sys/etimer.h \
+ /home/user/contiki-ng/os/sys/etimer.h \
+ /home/user/contiki-ng/os/sys/rtimer.h \
+ /home/user/contiki-ng/os/dev/watchdog.h \
+ /home/user/contiki-ng/arch/platform/cooja/./rtimer-arch.h \
+ /home/user/contiki-ng/arch/platform/cooja/./lib/simEnvChange.h \
+ /home/user/contiki-ng/arch/platform/cooja/./sys/cooja_mt.h \
+ /home/user/contiki-ng/arch/platform/cooja/./sys/cooja_mtarch.h \
+ /home/user/contiki-ng/os/sys/pt.h /home/user/contiki-ng/os/sys/clock.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-transport.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-endpoint.h \
+ /home/user/contiki-ng/os/net/ipv6/uip.h \
+ /home/user/contiki-ng/os/net/ipv6/uipopt.h \
+ /home/user/contiki-ng/os/net/ipv6/uipbuf.h \
+ /home/user/contiki-ng/os/net/linkaddr.h \
+ /home/user/contiki-ng/os/net/ipv6/tcpip.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-timer.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-transactions.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-observe.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-separate.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-observe-client.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-callback-api.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-request-state.h \
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-log.h \
+ /home/user/contiki-ng/os/sys/log.h \
+ /home/user/contiki-ng/os/sys/log-conf.h
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-callback-api.c :
+ /home/user/contiki-ng/os/net/app-layer/coap/module-macros.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-engine.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-constants.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-conf.h :
+ /home/user/contiki-ng/os/contiki.h :
+ /home/user/contiki-ng/arch/platform/cooja/./contiki-conf.h :
+ project-conf.h /home/user/contiki-ng/os/contiki-default-conf.h :
+ /home/user/contiki-ng/os/sys/process.h /home/user/contiki-ng/os/sys/pt.h :
+ /home/user/contiki-ng/os/sys/lc.h :
+ /home/user/contiki-ng/os/sys/lc-switch.h :
+ /home/user/contiki-ng/os/sys/cc.h /home/user/contiki-ng/os/sys/cc-gcc.h :
+ /home/user/contiki-ng/os/sys/autostart.h :
+ /home/user/contiki-ng/os/sys/process.h :
+ /home/user/contiki-ng/os/sys/timer.h :
+ /home/user/contiki-ng/os/sys/clock.h :
+ /home/user/contiki-ng/os/sys/ctimer.h :
+ /home/user/contiki-ng/os/sys/etimer.h :
+ /home/user/contiki-ng/os/sys/etimer.h :
+ /home/user/contiki-ng/os/sys/rtimer.h :
+ /home/user/contiki-ng/os/dev/watchdog.h :
+ /home/user/contiki-ng/arch/platform/cooja/./rtimer-arch.h :
+ /home/user/contiki-ng/arch/platform/cooja/./lib/simEnvChange.h :
+ /home/user/contiki-ng/arch/platform/cooja/./sys/cooja_mt.h :
+ /home/user/contiki-ng/arch/platform/cooja/./sys/cooja_mtarch.h :
+ /home/user/contiki-ng/os/sys/pt.h /home/user/contiki-ng/os/sys/clock.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-transport.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-endpoint.h :
+ /home/user/contiki-ng/os/net/ipv6/uip.h :
+ /home/user/contiki-ng/os/net/ipv6/uipopt.h :
+ /home/user/contiki-ng/os/net/ipv6/uipbuf.h :
+ /home/user/contiki-ng/os/net/linkaddr.h :
+ /home/user/contiki-ng/os/net/ipv6/tcpip.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-timer.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-transactions.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-observe.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-separate.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-observe-client.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-callback-api.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-request-state.h :
+ /home/user/contiki-ng/os/net/app-layer/coap/coap-log.h :
+ /home/user/contiki-ng/os/sys/log.h :
+ /home/user/contiki-ng/os/sys/log-conf.h :
---
+++ coap_sensors/oxygen_server/build/cooja/obj/nullradio.d
@@ -0,0 +1,8 @@
+build/cooja/obj/nullradio.o: /home/user/contiki-ng/os/dev/nullradio.c \
+ /home/user/contiki-ng/os/net/app-layer/coap/module-macros.h \
+ /home/user/contiki-ng/os/dev/nullradio.h \
+ /home/user/contiki-ng/os/dev/radio.h
+/home/user/contiki-ng/os/dev/nullradio.c :
+ /home/user/contiki-ng/os/net/app-layer/coap/module-macros.h :
+ /home/user/contiki-ng/os/dev/nullradio.h :
+ /home/user/contiki-ng/os/dev/radio.h :

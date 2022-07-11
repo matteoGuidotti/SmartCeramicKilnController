836fcfc8fe109bd8f411ff7f01259b80ada266cf
Matteo Guidotti <matteoguidotti98@gmail.com>
2022-07-11 11:25:50 -0400

    created application

---
+++ ceramicKilnController/src/main/java/iot/unipi/it/Collector.java
@@ -0,0 +1,12 @@
+package iot.unipi.it;
+
+import org.eclipse.californium.core.CoapServer;
+
+public class Collector extends CoapServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

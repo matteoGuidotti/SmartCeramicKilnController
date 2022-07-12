a9f4c4ec11daf685315551b273d22e5fc02939e5
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-07-12 05:56:45 -0400

    modificate configurazioni per rpl

---
+++ coap_sensors/fire_detector/build/cooja/obj/random.d
@@ -0,0 +1,6 @@
+build/cooja/obj/random.o: /home/user/contiki-ng/os/lib/random.c \
+ /home/user/contiki-ng/os/net/app-layer/coap/module-macros.h \
+ /home/user/contiki-ng/os/lib/random.h
+/home/user/contiki-ng/os/lib/random.c :
+ /home/user/contiki-ng/os/net/app-layer/coap/module-macros.h :
+ /home/user/contiki-ng/os/lib/random.h :

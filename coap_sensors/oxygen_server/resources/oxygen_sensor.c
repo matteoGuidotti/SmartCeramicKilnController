cbab85277794036b2ca785d26f6a55b24efb132d
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-07-14 19:53:11 +0200

    Update oxygen_sensor.c

--- coap_sensors/oxygen_server/resources/oxygen_sensor.c
+++ coap_sensors/oxygen_server/resources/oxygen_sensor.c
@@ -207,15 +207,15 @@ static void oxygen_event_handler(void)
 			LOG_INFO("Oxygen level: %f,low risk \n", oxygen_level);
 			if(leds_get() & (LEDS_NUM_TO_MASK(GREEN)) > 0)
 				break;
-			leds_set(LEDS_RED);
+			leds_set(MASK(LEDS_RED));
 			//leds_off(LEDS_NUM_TO_MASK(LEDS_RED));
 			//leds_toggle(LEDS_RED);
 			//leds_single_toggle(LEDS_GREEN);
 			break;
 		case MEDIUM_LOW:
 			printf("Oxygen level: %f, medium-low risk\n", oxygen_level);
-			leds_on(LEDS_RED);
-			leds_on(LEDS_GREEN);
+			leds_on(MASK(LEDS_RED));
+			leds_on(MASK(LEDS_GREEN));
 			//leds_on(LEDS_NUM_TO_MASK(LEDS_GREEN) );
 			//leds_on(LEDS_NUM_TO_MASK(LEDS_RED) );
 			//leds_set(MASK(LED))
@@ -224,8 +224,8 @@ static void oxygen_event_handler(void)
 			break;
 		case MEDIUM:
 			printf("Oxygen level: %f, medium risk\n", oxygen_level);
-			leds_on(LEDS_RED);
-			leds_on(LEDS_GREEN);
+			leds_on(MASK(LEDS_RED));
+			leds_on(MASK(LEDS_GREEN));
 			//leds_toggle(LEDS_RED);
 			//leds_toggle(LEDS_GREEN);
 			//leds_on(LEDS_NUM_TO_MASK(LEDS_GREEN) | LEDS_NUM_TO_MASK(LEDS_RED) );
@@ -234,7 +234,7 @@ static void oxygen_event_handler(void)
 			break;
 		case HIGH:
 			printf("Oxygen level: %f, high risk\n", oxygen_level);
-			leds_on(LEDS_GREEN);
+			leds_on(MASK(LEDS_GREEN));
 			//leds_set(LEDS_NUM_TO_MASK(LEDS_RED) );
 			//leds_off(LEDS_NUM_TO_MASK(LEDS_GREEN));
 			//leds_set(LEDS_NUM_TO_MASK(LEDS_RED));

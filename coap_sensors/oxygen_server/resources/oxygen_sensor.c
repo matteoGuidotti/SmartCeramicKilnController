1d7e6cd21270a8bc68dc20b1fee0cbe224b1e5f2
Clarissa Polidori <73442844+clarisa1997@users.noreply.github.com>
2022-07-14 20:17:37 +0200

    Update oxygen_sensor.c

--- coap_sensors/oxygen_server/resources/oxygen_sensor.c
+++ coap_sensors/oxygen_server/resources/oxygen_sensor.c
@@ -205,7 +205,7 @@ static void oxygen_event_handler(void)
 	switch (current_risk) {
 		case LOW:
 			LOG_INFO("Oxygen level: %f,low risk \n", oxygen_level);
-			if(leds_get() & (LEDS_NUM_TO_MASK(LEDS_RED)) <= 0)
+			if(leds_get() && (LEDS_NUM_TO_MASK(LEDS_RED)) <= 0)
 				leds_toggle(LEDS_RED);
 			//leds_toggle(LEDS_RED);
 			//leds_off(LEDS_NUM_TO_MASK(LEDS_RED));
